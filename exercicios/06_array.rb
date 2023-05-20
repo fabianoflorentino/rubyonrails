47a7799ad4983e1292a39e09256c893fdf9ba5ca
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-05-20 16:31:20 +0000

    Exercise 6: - Update documentation

--- exercicios/06_array.rb
+++ exercicios/06_array.rb
@@ -1,27 +1,35 @@
 # frozen_string_literal: true
 
+# Creates a line with 120 characters '-'.
 puts '-' * 120
 
+# Creates a array with list of arrays.
 v = [[11, 12, 13, 14, 15], [21, 22, 23, 24, 25], [31, 32, 33, 34, 35]]
 
+# Shows the array content using the each method to show content of the arrays
+# inside the array.
 v.each do |externo|
   externo.each do |interno|
-    puts interno
+    puts interno # Shows the content of the array.
   end
 end
 
 puts '-' * 120
 
+# Any string text is a array of characters.
 s = 'florentino'
 
+# Show the letter in the position 5 of the string.
 puts s[5]
 
 puts '-' * 120
 
+# Creates a array with list of numbers.
 v = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# Shows the array content using the each method.
 v.each do |item|
-  puts item
+  puts item # Shows the content of the array.
 end
 
 puts '-' * 120
