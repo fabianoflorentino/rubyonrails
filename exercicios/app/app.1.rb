58835bad9928dda814396c7c5051b03971d3a545
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-05-22 21:56:00 +0000

    Exercise 11: - Update documentation

--- exercicios/app/app.1.rb
+++ exercicios/app/app.1.rb
@@ -3,14 +3,18 @@
 require 'json'
 require_relative '../11_pessoa'
 
-nome = ARGV[0].to_s
-falando = 'Isso é um JSON file'
+nome = ARGV[0].to_s # If you want the program accepts one argument
+falando = 'Isso é um JSON file' # A text to falar method
 cumprimentando = "Olá, meu nome é #{nome}"
 
+# Instance a new object
 estudante = Pessoa.new
+
+# Creates a JSON
 pessoa = {
   falar: estudante.falar(falando), andar: estudante.andar,
   cumprimentar: estudante.cumprimentar(cumprimentando)
 }
 
+# Print the JSON
 puts JSON.pretty_generate(pessoa)
