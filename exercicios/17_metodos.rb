13c20dcd93f3231088b53d10022c77d733ba54ef
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-05-23 03:47:51 +0000

    Exercise 17

---
+++ exercicios/17_metodos.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Pessoa
+class Pessoa
+  def falar
+    'Olá pessoal'
+  end
+
+  def self.gritar(texto)
+    texto
+  end
+end
---
+++ exercicios/app/app.7.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../17_metodos'
+
+pessoa = Pessoa.new
+
+puts pessoa.falar
+puts Pessoa.gritar('Fala comigo!!!')
