a2bd0e57856a1117ec3582322d40dd68b38b5cf1
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-05-31 21:04:40 +0000

    Improve scrappercoin

--- crypto_wallet/lib/tasks/scrappercoin.rb
+++ crypto_wallet/lib/tasks/scrappercoin.rb
@@ -5,7 +5,19 @@ require "httparty"
 require "json"
 
 SCRAPPER_URL = "https://br.investing.com/crypto/currencies"
+SCRAPPER_URL_COIN = "https://br.investing.com/crypto"
 SCRAPPER_IMG_URL = "https://i-invdn-com.investing.com/ico_flags/80x80/v32"
+
+class CoinDTO
+  attr_reader :coin_name, :acronym, :var7, :url_image
+
+  def initialize(coin_name, acronym, var7, url_image)
+    @coin_name = coin_name
+    @acronym = acronym
+    @var7 = var7
+    @url_image = url_image
+  end
+end
 
 # Scrapper Coin
 class ScrapperCoin
@@ -20,49 +32,50 @@ class ScrapperCoin
     @rows
   end
 
-  def json_scraper_data_coin
+  def scrapper_filter(row)
+    coin_name = row.css(".left.bold.elp.name.cryptoName.first.js-currency-name").text.strip
+    acronym = row.css(".left.noWrap.elp.symb.js-currency-symbol").text.strip
+    var7 = row.css(".js-currency-change-7d.greenFont", ".js-currency-change-7d.redFont").text.strip
+    url_image = "#{SCRAPPER_IMG_URL}/#{coin_name.downcase}.png".tr(" ", "-")
+
+    url_consult = HTTParty.get(url_image)
+    if url_consult.code >= 400
+      url_image = "#{SCRAPPER_IMG_URL}/#{coin_name.downcase}-new.png".tr(" ", "-")
+    end
+
+    CoinDTO.new(coin_name, acronym, var7, url_image)
+  end
+
+  def json_scraper_data_coin(numer_of_coins)
     rows = scraper_data_coin
 
-    rows.take(30).each do |row|
-      @coin_name = row.css(".left.bold.elp.name.cryptoName.first.js-currency-name").text.strip
-      @acronym = row.css(".left.noWrap.elp.symb.js-currency-symbol").text.strip
-      @var7 = row.css(".js-currency-change-7d.greenFont", ".js-currency-change-7d.redFont").text.strip
-      @url_image = "#{SCRAPPER_IMG_URL}/#{@coin_name.downcase}.png".tr(" ", "-")
+    rows.take(numer_of_coins).map do |row|
+      coin_filter = scrapper_filter(row)
 
-      results = {
-        name: @coin_name,
-        acronym: @acronym,
-        var7: @var7,
-        image: @url_image
+      {
+        name: coin_filter.coin_name,
+        acronym: coin_filter.acronym,
+        var7: coin_filter.var7,
+        image: coin_filter.url_image
       }
-
-      puts JSON.pretty_generate(results)
     end
   end
 
-  def db_scraper_data_coin
+  def db_scraper_data_coin(numer_of_coins)
     rows = scraper_data_coin
 
-    rows.take(30).each do |row|
-      @coin_name = row.css(".left.bold.elp.name.cryptoName.first.js-currency-name").text.strip
-      @acronym = row.css(".left.noWrap.elp.symb.js-currency-symbol").text.strip
-      @var7 = row.css(".js-currency-change-7d.greenFont", ".js-currency-change-7d.redFont").text.strip
-      @url_image = "#{SCRAPPER_IMG_URL}/#{@coin_name.downcase}.png".tr(" ", "-")
+    rows.take(numer_of_coins).each do |row|
+      coin_filter = scrapper_filter(row)
 
-      crypto_currency = Coin.find_by(description: @coin_name)
+      crypto_currency = Coin.find_or_initialize_by(description: coin_filter.coin_name)
 
-      Coin.find_or_create_by!(
-        description: @coin_name,
-        acronym: @acronym,
-        url_image: @url_image,
-        percentagechange7d: @var7
-      ) unless crypto_currency
+      crypto_currency.attributes = {
+        acronym: coin_filter.acronym,
+        url_image: coin_filter.url_image,
+        percentagechange7d: coin_filter.var7
+      }
 
-      if crypto_currency
-        crypto_currency.update(acronym: @acronym)
-        crypto_currency.update(url_image: @url_image)
-        crypto_currency.update(percentagechange7d: @var7)
-      end
+      crypto_currency.save!
     end
   end
 end
