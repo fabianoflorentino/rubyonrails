875a5d790bcc7efe66ec3459659352d321fd066f
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2023-06-04 03:05:30 +0000

    Improved sidekiq configuration

--- crypto_wallet/config/initializers/sidekiq.rb
+++ crypto_wallet/config/initializers/sidekiq.rb
@@ -1,11 +1,16 @@
-require_relative "../../app/sidekiq/getdatacoin_job"
-
 Sidekiq.configure_server do |config|
+  schedule_file = "../../config/sidekiq.yml"
+
   config.redis = { url: REDIS_URL }
+
+  if File.exist?(schedule_file)
+    config.on(:startup) do
+      Sidekiq.schedule = YAML.load_file(File.expand_path(schedule_file, __FILE__))
+      SidekiqScheduler::Scheduler.instance.reload_schedule!
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: REDIS_URL }
 end
-
-GetdatacoinJob.perform_in(10.minutes)
